772be7fe217dc8ba11de13316fe7d8fb3c67dc2b
joaolucianno <joaolucianno@gmail.com>
2021-03-23 01:34:39 +0000

    Finished Spring xml

--- src/main/java/org/academiadecodigo/javabank/App.java
+++ src/main/java/org/academiadecodigo/javabank/App.java
@@ -2,24 +2,8 @@ package org.academiadecodigo.javabank;
 
 import org.academiadecodigo.javabank.controller.Controller;
 import org.academiadecodigo.javabank.controller.LoginController;
-import org.academiadecodigo.javabank.controller.NewAccountController;
-import org.academiadecodigo.javabank.model.Customer;
-import org.academiadecodigo.javabank.model.account.Account;
-import org.academiadecodigo.javabank.persistence.JpaBootstrap;
-import org.academiadecodigo.javabank.persistence.TransactionManager;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaAccountDao;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaCustomerDao;
-import org.academiadecodigo.javabank.persistence.jpa.JpaSessionManager;
-import org.academiadecodigo.javabank.persistence.jpa.JpaTransactionManager;
-import org.academiadecodigo.javabank.services.AccountServiceImpl;
-import org.academiadecodigo.javabank.services.AuthServiceImpl;
-import org.academiadecodigo.javabank.services.CustomerServiceImpl;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-import javax.persistence.EntityManagerFactory;
 
 public class App {
 
@@ -27,42 +11,9 @@ public class App {
 
         ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
                 "src/main/resources/META-INF/spring-config.xml");
-        LoginController controller = applicationContext.getBean(LoginController.class);
+        Controller controller = applicationContext.getBean(LoginController.class);
         controller.init();
-
-
-        //JpaBootstrap jpa = new JpaBootstrap();
-        //EntityManagerFactory emf = jpa.start();
-
-        //JpaSessionManager sm = new JpaSessionManager(emf);
-        //TransactionManager tx = new JpaTransactionManager(sm);
-
-        //App app = new App();
-        //app.bootStrap(tx, sm);
-
-        //jpa.stop();
 
     }
 
-    private void bootStrap(TransactionManager tx, JpaSessionManager sm) {
-
-        AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.setAccountDao(new JpaAccountDao(sm));
-        accountService.setTransactionManager(tx);
-
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        customerService.setCustomerDao(new JpaCustomerDao(sm));
-        customerService.setTransactionManager(tx);
-
-        Bootstrap bootstrap = new Bootstrap();
-
-        bootstrap.setAuthService(new AuthServiceImpl());
-        bootstrap.setAccountService(accountService);
-        bootstrap.setCustomerService(customerService);
-
-        Controller controller = bootstrap.wireObjects();
-
-        // start application
-        controller.init();
-    }
 }
